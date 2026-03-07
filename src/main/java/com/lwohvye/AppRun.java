14cef4033c0c9d0c36d216bb78ac7cdb94dc7a7a
Hongyan, Wang <39266195+lWoHvYe@users.noreply.github.com>
2026-03-07 08:10:39 +0800

    update

--- src/main/java/com/lwohvye/AppRun.java
+++ src/main/java/com/lwohvye/AppRun.java
@@ -22,7 +22,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,7 +34,6 @@ import org.springframework.web.bind.annotation.RestController;
 @Hidden
 @SpringBootApplication // 核心配置类
 @EnableTransactionManagement // 开启事务
-@EnableRetry //开启重试机制
 @EnableConfigurationProperties //开启 @ConfigurationProperties 注解
 // 以下注解在其他类上有配置
 // @EnableCaching
